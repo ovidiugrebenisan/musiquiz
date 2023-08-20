d81006ad3044d84ad63a5f4a0a5a2ed574bf19b3
Ovidiu Grebenisan <ovidiu.grebenisan@gmail.com>
2023-08-21 00:36:26 +0300

    added random background image generation

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -3,14 +3,49 @@ import Link from "next/link";
 import { useAtomValue } from "jotai/react";
 import { artistAtom } from "./Search";
 import { AnswerButton } from "~/components/AnswerButton";
+import { type release } from "@prisma/client";
+import { useEffect, useState } from "react";
 
-function getRandomNumber(lowerLimit: number, upperLimit: number, exception: number): number {
+function getRandomElement(arr: release[]) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return randomIndex;
+}
+
+export interface CoverArt {
+  images?: (ImagesEntity)[] | null;
+  release: string;
+}
+export interface ImagesEntity {
+  approved: boolean;
+  back: boolean;
+  comment: string;
+  edit: number;
+  front: boolean;
+  id: number;
+  image: string;
+  thumbnails: Thumbnails;
+  types?: (string)[] | null;
+}
+export interface Thumbnails {
+  250: string;
+  500: string;
+  1200: string;
+  large: string;
+  small: string;
+}
+
+
+function getRandomNumber(
+  lowerLimit: number,
+  upperLimit: number,
+  exception: number,
+): number {
   const range: number = upperLimit - lowerLimit + 1;
 
   let randomNumber: number = Math.floor(Math.random() * range) + lowerLimit;
 
   if (randomNumber === exception) {
-    randomNumber = getRandomNumber(lowerLimit, upperLimit, exception)
+    randomNumber = getRandomNumber(lowerLimit, upperLimit, exception);
   }
   return randomNumber;
 }
@@ -33,24 +68,72 @@ function shuffle<T>(array: T[]): T[] {
 }
 
 export default function Quiz() {
+  const [coverArt, setCoverArt] = useState<string | null>(null)
   const artistPicked = useAtomValue(artistAtom);
-  const albumYear = api.mbdb.getAlbumStartYear.useQuery(artistPicked, {
+  const artistData = api.mbdb.getAristData.useQuery(artistPicked, {
     staleTime: Infinity,
     retry: false,
-  }).data as number;
+  }).data;
+
+  let albumYear = 0;
+  let artistId = 0;
+
+  if (artistData && artistData.begin_date_year) {
+    albumYear = artistData.begin_date_year;
+  }
+
+  if (artistData && artistData.id) {
+    artistId = artistData.id;
+  }
+
+  const artistReleases = api.mbdb.getArtistReleases.useQuery(artistId, {
+    staleTime: Infinity,
+    retry: false,
+    enabled: !!artistId
+  }).data;
+  let randomIndex = 0;
+  let release;
+  if (artistReleases) {
+    randomIndex = getRandomElement(artistReleases);
+    release = artistReleases[randomIndex];
+
+  }
+
+  const releaseID = release?.gid
+  const coverArtURL = "https://coverartarchive.org/release/" + releaseID
+
+
+  useEffect(() => {
+    // Ensure there's a releaseID before fetching
+    if (releaseID) {
+        fetch(coverArtURL)
+            .then((res) => res.json())
+            .then((data: CoverArt) => {
+                // Assuming the first image is the one you want.
+                if (data.images && data.images.length > 0) {
+                  const imageUrl = data.images?.[0]?.image ?? null;
+                    setCoverArt(imageUrl);
+                }
+            })
+            .catch((e) => {
+                console.error("Error fetching cover art:", e);
+            });
+    }
+}, []);
+
+  console.log(coverArt)
 
 
 
-
-    const higher_year = getRandomNumber(albumYear - 1, albumYear + 1, albumYear);
-    const lower_year = getRandomNumber(albumYear - 2, albumYear - 7, albumYear);
-    const another_year = getRandomNumber(albumYear + 2, albumYear + 7, albumYear);
-  
+  const higher_year = getRandomNumber(albumYear - 1, albumYear + 1, albumYear);
+  const lower_year = getRandomNumber(albumYear - 2, albumYear - 7, albumYear);
+  const another_year = getRandomNumber(albumYear + 2, albumYear + 7, albumYear);
 
   const answers: number[] = [albumYear, higher_year, lower_year, another_year];
   const shuffledArray = shuffle(answers);
   return (
-    <div className="relative h-screen w-screen bg-quiz bg-cover saturate-0">
+    <div className="relative h-screen w-screen bg-cover saturate-0"
+      style={{ backgroundImage: `url(${coverArt})`}}>
       <div className="absolute h-full w-full bg-black bg-opacity-25"> </div>
       <Link
         href="/"
--- src/server/api/routers/mbdb.ts
+++ src/server/api/routers/mbdb.ts
@@ -2,18 +2,41 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const getArtistData = createTRPCRouter({
-  getAlbumStartYear: publicProcedure.input(z.string()).query( async ({ ctx, input }) => {
-    const albumStartYear = await ctx.prisma.artist.findFirst({
-      where: {
-        name: input,
-      },
-    });
+  getAristData: publicProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      const artistData = await ctx.prisma.artist.findFirst({
+        where: {
+          name: input,
+        },
+        select: {
+          id: true,
+          begin_date_year: true,
+        }
+      });
 
-    if (albumStartYear?.begin_date_year) {
-      return albumStartYear.begin_date_year
-    }
+      if (artistData?.begin_date_year && artistData.id) {
+        return {
+          id: artistData.id,
+          begin_date_year: artistData.begin_date_year
+        }
+      }
 
-    return null
-  }),
-},
-);
+      return null;
+    }),
+
+  getArtistReleases: publicProcedure
+    .input(z.number())
+    .query( async ({ctx, input}) => {
+      const artistReleases = await ctx.prisma.release.findMany({
+        where: {
+          artist_credit: input
+        }
+      });
+
+      if (artistReleases) {
+        return artistReleases
+      }
+      return null
+    })
+});
