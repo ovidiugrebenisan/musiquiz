4812cfd38de343e0b8a8fd5a96b6d5c89d295536
Ovidiu Grebenisan <ovidiu.grebenisan@gmail.com>
2023-09-28 19:34:30 +0300

    removed tanstack query tools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,10 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <ReactQueryDevtools initialIsOpen={true} />
       <Component {...pageProps} />
     </ClerkProvider>
   );
